2d342e229dadf22e134a26d3ee799740936572b6
fynity-innovations <fynityinnovations@gmail.com>
2026-03-22 15:00:53 +0530

    activate

--- app/(auth)/activate/page.tsx
+++ app/(auth)/activate/page.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import api from "@/lib/api"
+import { Logo } from "@/components/logo"
 
 function ActivateContent() {
   const router = useRouter()
